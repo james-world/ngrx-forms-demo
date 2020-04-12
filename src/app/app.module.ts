39927a4662eab5ab53c759295e10a02bed8fdff3
James World <james-world@users.noreply.github.com>
2020-04-12 23:29:39 +0100

    Add ngrx, configure modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgrxFormsModule } from 'ngrx-forms';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgrxFormsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
