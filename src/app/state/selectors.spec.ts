0791afc40201c7d2bc3912c6f5bd0c8dc81d84a0
James World <james-world@users.noreply.github.com>
2020-04-15 00:26:10 +0100

    Add selector test

---
+++ src/app/state/selectors.spec.ts
@@ -0,0 +1,25 @@
+import * as selectors from './selectors';
+import { createFormGroupState } from 'ngrx-forms';
+import { Team } from './reducer';
+
+const initialTeam: Team = {
+  name: 'Albion',
+  maxSubs: 2,
+  players: [
+    { name: 'Tommy', isSub: false },
+    { name: 'Harry', isSub: true },
+  ],
+};
+
+const initialState = {
+  team: initialTeam,
+  teamForm: createFormGroupState<Team>('TeamForm', initialTeam),
+};
+
+describe('My Selectors', () => {
+  it('should calculate number of subs', () => {
+    expect(
+      selectors.selectSubCount.projector(initialState.teamForm.value.players)
+    ).toEqual(1);
+  });
+});
